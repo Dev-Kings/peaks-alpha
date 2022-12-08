38aae90a9e10034be9b1386298a59bdfbab713e3
Dev-Kings <kingasiadavid41@gmail.com>
2022-12-08 11:54:05 +0300

    added testimonial-commented-out, minor changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -188,6 +188,24 @@
     })
   }
 
+    /**
+   * Testimonials Slider
+   */
+     new Swiper('.testimonials-slider', {
+      speed: 600,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      }
+    });
+
   /**
    * Porfolio isotope and filter
    */
